2bc2f6f250a51e3f58a29bf06bea656fc1dc6e65
Thales-Vicente <thalesvicentebem10@hotmail.com>
2025-02-24 11:10:01 -0300

    Layout Created

---
+++ app/src/main/java/com/example/asteroidradar/Adapter.kt
@@ -0,0 +1,4 @@
+package com.example.asteroidradar
+
+class Adapter() {
+}
---
+++ app/src/main/java/com/example/asteroidradar/MainFragment.kt
@@ -0,0 +1,24 @@
+package com.example.asteroidradar
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.asteroidradar.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
